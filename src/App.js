6382a3eba88de4e34a4dc4457397e1a48e363341
ext.wangmei5 <ext.wangmei5@jd.com>
2020-11-26 15:21:49 +0800

    feat: antd子定义主题/引入路由等

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './App.css';
+import { Button } from 'antd';
+import './App.less';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <h1>Learn React</h1>
+          <Button type="primary">我是antd组件哦</Button>
       </header>
     </div>
   );
