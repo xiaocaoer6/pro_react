e8fb23b31683564e62dc799458b8d92d74108d43
ext.wangmei5 <ext.wangmei5@jd.com>
2021-01-07 19:04:12 +0800

    feat: 登录静态页面

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,66 @@
 import React, { Component } from 'react'
+import logo from '../../assets/logogif.gif'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './index.less'
 
-export default class Login  extends Component {
-  render(){
-    return (
-      <div>
-        login 登录组件
+const Login = () => {
+
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className="login-wrap">
+      <div className="header">
+        <img src={logo} alt="logo" />
+        <h2>商品管理系统</h2>
       </div>
-    )
-  }
+      <div className="content">
+        <h2 className="title">
+          用户登录
+        </h2>
+        <Form
+          name="normal_login"
+          className="login-form"
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: '请输入用户名！',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: '请输入密码！',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="密码"
+            />
+          </Form.Item>
+
+          <Form.Item className="login-btn">
+            <Button type="primary" htmlType="submit" className="login-form-button" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  )
 }
+
+export default Login
